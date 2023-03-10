edf17931f9d422b7f9ad059a2a72cadc559cb99b
LeeGiCheol <leegicheolgc@gmail.com>
2023-03-10 23:37:17 +0900

    Update: 이 테스트 코드는 testMethod만을 위한 것이 아니기 때문에 이름을 run으로 수정하고, 생성자를 통해 테스트를 수행할 메서드 명을 전달한다. (Refactoring 2)
    최초에 작성한 테스트 코드를 뽑아 TestCaseTest 라는 클래스를 만든다.

--- src/cheeolee/xunit/XUnitTest.java
+++ src/cheeolee/xunit/XUnitTest.java
@@ -1,18 +1,12 @@
 package cheeolee.xunit;
 
 /**
- * 이 테스트 코드는 testMethod만을 위한 것이 아니기 때문에 이름을 run으로 수정하고,
- * 생성자를 통해 테스트를 수행할 메서드 명을 전달한다.
+ * 기존의 테스트를 테스트 할 수 있도록 TestCaseTest 클래스로 옮긴다.
  */
 public class XUnitTest {
 
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun("testMethod");
-        System.out.println(wasRun.wasRun); // false
-
-        wasRun.run();
-
-        System.out.println(wasRun.wasRun); // true
+        new TestCaseTest("testRunning").run();
     }
 
 }
